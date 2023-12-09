450f366a082c4ba53342bae1ccc53ba08dccfbdc
Habil-Ndiamoun <85562269+Habil-Ndiamoun@users.noreply.github.com>
2023-12-08 19:41:59 -0500

    Création des classes pour représenter les entités Livre et Exemplaire

---
+++ Gestion_Livres/Models/Exemplaire.cs
@@ -0,0 +1,9 @@
+﻿namespace Gestion_Livres.Models
+{
+    public class Exemplaire
+    {
+        public int ExemplaireId { get; set; }
+        public bool EstEmprunte { get; set; }
+        public int LivreId { get; set; }
+    }
+}
---
+++ Gestion_Livres/Models/Livre.cs
@@ -0,0 +1,9 @@
+﻿namespace Gestion_Livres.Models
+{
+    public class Livre
+    {
+        public int LivreId { get; set; }
+        public string? Titre { get; set; }   
+        public ICollection<Exemplaire>? Exemplaires { get; set; }
+    }
+}
